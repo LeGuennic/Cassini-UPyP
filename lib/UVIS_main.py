9bc95a15a5a02a85d8491190734a52579243178a
LeGuennic <nathan.leguennic@gmail.com>
2025-05-21 10:08:30 +0200

    fix: Add pixel_LOS attribute to UVIS_bin class to keep pixel_properties information in the bin

--- lib/UVIS_main.py
+++ lib/UVIS_main.py
@@ -642,6 +642,7 @@ class UVIS_bin:
 
         self.number_per_bin = np.zeros_like(self.bins, dtype=int)
 
+        self.pixel_LOS = np.copy(uvis_obs.pixel_LOS)
         self.bin_LOS = np.zeros_like(self.bins, dtype=uvis_obs.pixel_LOS.dtype)
 
         self.observation = uvis_obs.name
